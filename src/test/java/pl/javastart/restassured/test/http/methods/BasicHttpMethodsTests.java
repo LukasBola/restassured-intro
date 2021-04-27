afdbbe45f1679378969046776cb4dd1f8b2f0849
Luk <lukasz.bola@icloud.com>
2021-04-27 13:49:24 +0200

    commit - POJO

--- src/test/java/pl/javastart/restassured/test/http/methods/BasicHttpMethodsTests.java
+++ src/test/java/pl/javastart/restassured/test/http/methods/BasicHttpMethodsTests.java
@@ -1,6 +1,12 @@
 package pl.javastart.restassured.test.http.methods;
 
 import org.testng.annotations.Test;
+import pl.javastart.main.pojo.Category;
+import pl.javastart.main.pojo.Pet;
+import pl.javastart.main.pojo.Tag;
+
+import java.util.Collections;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -22,24 +28,40 @@ public class BasicHttpMethodsTests {
      **/
     @Test
     public void givenPetWhenPostPetThenPetIsCreatedTest() {
-        String pet = "{\n" +
-                "  \"id\": 56,\n" +
-                "  \"category\": {\n" +
-                "    \"id\": 1,\n" +
-                "    \"name\": \"dogs\"\n" +
-                "  },\n" +
-                "  \"name\": \"Stafik\",\n" +
-                "  \"photoUrls\": [\n" +
-                "    \"http://staffik.pl/wp-content/uploads/2016/06/2014_BRUNO-staffik-z-Bullerbyn-768x621.jpg\"\n" +
-                "  ],\n" +
-                "  \"tags\": [\n" +
-                "    {\n" +
-                "      \"id\": 1,\n" +
-                "      \"name\": \"dogs-category\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"status\": \"available\"\n" +
-                "}";
+//        String pet = "{\n" +
+//                "  \"id\": 56,\n" +
+//                "  \"category\": {\n" +
+//                "    \"id\": 1,\n" +
+//                "    \"name\": \"dogs\"\n" +
+//                "  },\n" +
+//                "  \"name\": \"Stafik\",\n" +
+//                "  \"photoUrls\": [\n" +
+//                "    \"http://staffik.pl/wp-content/uploads/2016/06/2014_BRUNO-staffik-z-Bullerbyn-768x621.jpg\"\n" +
+//                "  ],\n" +
+//                "  \"tags\": [\n" +
+//                "    {\n" +
+//                "      \"id\": 1,\n" +
+//                "      \"name\": \"dogs-category\"\n" +
+//                "    }\n" +
+//                "  ],\n" +
+//                "  \"status\": \"available\"\n" +
+//                "}";
+
+        Category category = new Category();
+        category.setId(1);
+        category.setName("dogs");
+
+        Tag tag = new Tag();
+        tag.setId(1);
+        tag.setName("dogs-category");
+
+        Pet pet = new Pet();
+        pet.setId(56);
+        pet.setCategory(category);
+        pet.setName("Stafik_666");
+        pet.setPhotoUrls(Collections.singletonList("http://staffik.pl/wp-content/uploads/2016/06/2014_BRUNO-staffik-z-Bullerbyn-768x621.jpg"));
+        pet.setTags(Collections.singletonList(tag));
+        pet.setStatus("available");
 
         given().log().all().
                 body(pet).contentType("application/json").
