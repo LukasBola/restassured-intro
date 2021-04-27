d20d782a8e08a7c191cc9c3d626b3309552f1dca
Luk <lukasz.bola@icloud.com>
2021-04-27 14:25:42 +0200

    commit - POJO

---
+++ src/main/java/pl/javastart/main/pojo/Category.java
@@ -0,0 +1,28 @@
+
+package pl.javastart.main.pojo;
+
+import javax.annotation.Generated;
+
+@Generated("jsonschema2pojo")
+public class Category {
+
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
