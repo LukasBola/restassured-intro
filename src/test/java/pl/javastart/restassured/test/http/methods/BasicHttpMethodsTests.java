78e958f236064837eaa5ae44a61075a7eb840582
Luk <lukasz.bola@icloud.com>
2021-04-27 14:25:07 +0200

    commit - POJO

--- src/test/java/pl/javastart/restassured/test/http/methods/BasicHttpMethodsTests.java
+++ src/test/java/pl/javastart/restassured/test/http/methods/BasicHttpMethodsTests.java
@@ -13,39 +13,21 @@ import static io.restassured.RestAssured.given;
 public class BasicHttpMethodsTests {
 
     /**
-     GET
+     * GET
      **/
     @Test
     public void givenExistingPetIdWhenGetPetThenReturnPetTest() {
         given().log().all().
-                pathParam("petId","56").
+                pathParam("petId", "58").
                 when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{petId}").
                 then().log().all().statusCode(200);
     }
 
     /**
-     POST
+     * POST
      **/
     @Test
     public void givenPetWhenPostPetThenPetIsCreatedTest() {
-//        String pet = "{\n" +
-//                "  \"id\": 56,\n" +
-//                "  \"category\": {\n" +
-//                "    \"id\": 1,\n" +
-//                "    \"name\": \"dogs\"\n" +
-//                "  },\n" +
-//                "  \"name\": \"Stafik\",\n" +
-//                "  \"photoUrls\": [\n" +
-//                "    \"http://staffik.pl/wp-content/uploads/2016/06/2014_BRUNO-staffik-z-Bullerbyn-768x621.jpg\"\n" +
-//                "  ],\n" +
-//                "  \"tags\": [\n" +
-//                "    {\n" +
-//                "      \"id\": 1,\n" +
-//                "      \"name\": \"dogs-category\"\n" +
-//                "    }\n" +
-//                "  ],\n" +
-//                "  \"status\": \"available\"\n" +
-//                "}";
 
         Category category = new Category();
         category.setId(1);
@@ -70,66 +52,67 @@ public class BasicHttpMethodsTests {
     }
 
     /**
-     PUT
+     * PUT
      **/
     @Test
-    public void givenExistingPetWhenUpdatePetNameThenPetIsChangedTest(){
-        String updatePet = "{\n" +
-                "  \"id\": 56,\n" +
-                "  \"category\": {\n" +
-                "    \"id\": 1,\n" +
-                "    \"name\": \"dogs\"\n" +
-                "  },\n" +
-                "  \"name\": \"Stafik_update_name\",\n" +
-                "  \"photoUrls\": [\n" +
-                "    \"http://staffik.pl/wp-content/uploads/2016/06/2014_BRUNO-staffik-z-Bullerbyn-768x621.jpg\"\n" +
-                "  ],\n" +
-                "  \"tags\": [\n" +
-                "    {\n" +
-                "      \"id\": 1,\n" +
-                "      \"name\": \"dogs-category\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"status\": \"available\"\n" +
-                "}";
+    public void givenExistingPetWhenUpdatePetNameThenPetIsChangedTest() {
+
+        Category category = new Category();
+        category.setId(1);
+        category.setName("dogs");
+
+        Tag tag = new Tag();
+        tag.setId(1);
+        tag.setName("dogs-category");
+
+        Pet pet = new Pet();
+        pet.setId(56);
+        pet.setCategory(category);
+        pet.setName("Stafik_update_name_2");
+        pet.setPhotoUrls(Collections.singletonList("http://staffik.pl/wp-content/uploads/2016/06/2014_BRUNO-staffik-z-Bullerbyn-768x621.jpg"));
+        pet.setTags(Collections.singletonList(tag));
+        pet.setStatus("available");
+
 
         given().log().all().
-                body(updatePet).contentType("application/json").
+                body(pet).contentType("application/json").
                 when().put("https://swaggerpetstore.przyklady.javastart.pl/v2/pet").
                 then().log().all().statusCode(200);
     }
 
     /**
-     DELETE
+     * DELETE
      **/
     @Test
-    public void givenExistingPetIdWhenDeletingPetThenIsDeletedTest(){
-//        String deletePet = "{\n" +
-//                "  \"id\": 56,\n" +
-//                "  \"category\": {\n" +
-//                "    \"id\": 1,\n" +
-//                "    \"name\": \"dogs\"\n" +
-//                "  },\n" +
-//                "  \"name\": \"Stafik_delete\",\n" +
-//                "  \"photoUrls\": [\n" +
-//                "    \"http://staffik.pl/wp-content/uploads/2016/06/2014_BRUNO-staffik-z-Bullerbyn-768x621.jpg\"\n" +
-//                "  ],\n" +
-//                "  \"tags\": [\n" +
-//                "    {\n" +
-//                "      \"id\": 1,\n" +
-//                "      \"name\": \"dogs-category\"\n" +
-//                "    }\n" +
-//                "  ],\n" +
-//                "  \"status\": \"available\"\n" +
-//                "}";
-//
-//        given().log().all().body(deletePet).contentType("application/json").
-//                when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet").
-//                then().log().all().statusCode(200);
+    public void givenExistingPetIdWhenDeletingPetThenIsDeletedTest() {
 
-        given().log().all().
-                pathParam("petId","56").
-                when().delete("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{petId}").
+        Category category = new Category();
+        category.setId(1);
+        category.setName("dogs");
+
+
+        Tag tag = new Tag();
+        tag.setId(1);
+        tag.setName("dogs-category");
+
+        Pet pet = new Pet();
+        pet.setId(58);
+        pet.setCategory(category);
+        pet.setName("Stafik_delete");
+        pet.setPhotoUrls(Collections.singletonList("http://staffik.pl/wp-content/uploads/2016/06/2014_BRUNO-staffik-z-Bullerbyn-768x621.jpg"));
+        pet.setTags(Collections.singletonList(tag));
+        pet.setStatus("available");
+
+        given().log().all().body(pet).contentType("application/json").
+                when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet").
                 then().log().all().statusCode(200);
+
+        given().
+                log().all().
+                pathParam("petId", pet.getId())
+                .when().
+                delete("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{petId}")
+                .then().
+                log().all().statusCode(200);
     }
 }
