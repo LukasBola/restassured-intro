df15d70cf85fe8abdce87af2ca8657e19f592dc4
Luk <lukasz.bola@icloud.com>
2021-04-27 16:01:49 +0200

    commit - POJO zadanie 5.6

--- src/test/java/pl/javastart/restassured/test/tasks/UserUpdateTests.java
+++ src/test/java/pl/javastart/restassured/test/tasks/UserUpdateTests.java
@@ -5,26 +5,38 @@ import pl.javastart.main.pojo.user.User;
 
 import static io.restassured.RestAssured.given;
 
-public class UserCreationTests {
+public class UserUpdateTests {
 
     @Test
-    public void givenCorrectUserDataWhenCreateUserThenUserIsCreatedTest() {
+    public void givenCorrectUserDataWhenFirstNameLastNameAreUpdatedThenUserDataIsUpdatedTest() {
 
         User user = new User();
-        user.setId(156);
+        user.setId(57);
         user.setUsername("lukasss");
-        user.setFirstName("luk");
-        user.setLastName("asssdr");
+        user.setFirstName("oldfirsname");
+        user.setLastName("oldlastname");
         user.setEmail("lub@ass.pl");
         user.setPassword("password_strong");
         user.setPhone("+666 666 666");
         user.setUserStatus(666);
+
 
         given().log().all().
                 body(user).
                 contentType("application/json").
                 when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/user").
                 then().log().all().statusCode(200);
+
+        user.setFirstName("Jan");
+        user.setLastName("Mieszko I");
+
+        given().log().all().
+                pathParam("username", user.getUsername()).
+                body(user).
+                contentType("application/json").
+                when().put("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}").
+                then().log().all().statusCode(200);
+
 
         given().log().all().
                 pathParam("username", user.getUsername()).
