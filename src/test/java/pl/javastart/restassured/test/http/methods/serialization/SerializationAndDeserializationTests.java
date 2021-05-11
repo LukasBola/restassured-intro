325fde9c81a0a9aafb999de00a2de5de8b27107d
Luk <lukasz.bola@icloud.com>
2021-05-11 17:24:17 +0200

    commit - wbudowane asercje - assertThat() - lekcja

--- src/test/java/pl/javastart/restassured/test/http/methods/serialization/SerializationAndDeserializationTests.java
+++ src/test/java/pl/javastart/restassured/test/http/methods/serialization/SerializationAndDeserializationTests.java
@@ -5,14 +5,15 @@ import org.testng.asserts.SoftAssert;
 import pl.javastart.main.pojo.Category;
 import pl.javastart.main.pojo.Pet;
 import pl.javastart.main.pojo.Tag;
-
-import static org.testng.Assert.assertEquals;
+import pl.javastart.restassured.test.TestBase;
 
 import java.util.Collections;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.testng.Assert.assertEquals;
 
-public class SerializationAndDeserializationTests {
+public class SerializationAndDeserializationTests extends TestBase {
 
     /**
      * POST
@@ -36,9 +37,9 @@ public class SerializationAndDeserializationTests {
         pet.setTags(Collections.singletonList(tag));
         pet.setStatus("available");
 
-        Pet actualPet = given().log().all().body(pet).contentType("application/json")
+        Pet actualPet = given().body(pet).contentType("application/json")
                 .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
-                .then().log().all().statusCode(200)
+                .then().statusCode(200)
                 .extract().as(Pet.class);
 
         assertEquals(actualPet.getId(), pet.getId(), "Pet id");
@@ -48,6 +49,43 @@ public class SerializationAndDeserializationTests {
         assertEquals(actualPet.getTags().get(0).getId(), pet.getTags().get(0).getId(), "Pet tag id");
         assertEquals(actualPet.getTags().get(0).getName(), pet.getTags().get(0).getName(), "Pet tag name");
         assertEquals(actualPet.getStatus(), pet.getStatus(), "Pet status");
+    }
+
+    @Test
+    public void givenPetWhenPostPetThenPetIsCreatedTest2() {
+
+        Category category = new Category();
+        category.setId(1);
+        category.setName("dogs");
+
+        Tag tag = new Tag();
+        tag.setId(1);
+        tag.setName("dogs-category");
+
+        Pet pet = new Pet();
+        pet.setId(56);
+        pet.setCategory(category);
+        pet.setName("Stafik_777");
+        pet.setPhotoUrls(Collections.singletonList("http://staffik.pl/wp-content/uploads/2016/06/2014_BRUNO-staffik-z-Bullerbyn-768x621.jpg"));
+        pet.setTags(Collections.singletonList(tag));
+        pet.setStatus("available");
+
+        //Metoda różni się od swojej poprzedniczki tylko rodzajem asercji
+        given().body(pet).contentType("application/json")
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then()
+                .assertThat().statusCode(200)
+                .assertThat().contentType("application/json")
+                .assertThat().body(
+                "id", equalTo(56),
+                "category.id", equalTo(1),
+                "category.name", equalTo("dogs"),
+                "name", equalTo("Stafik_777"),
+                "photoUrls[0]", equalTo("http://staffik.pl/wp-content/uploads/2016/06/2014_BRUNO-staffik-z-Bullerbyn-768x621.jpg"),
+                "tags[0].id", equalTo(1),
+                "tags[0].name", equalTo("dogs-category"),
+                "status", equalTo("available")
+        );
     }
 
     @Test
@@ -70,15 +108,15 @@ public class SerializationAndDeserializationTests {
         pet.setTags(Collections.singletonList(tag));
         pet.setStatus("available");
 
-        Pet actualPet = given().log().all().
+        Pet actualPet = given().
                 body(pet).contentType("application/json").
                 when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet").
-                then().log().all().statusCode(200).extract().as(Pet.class);
+                then().statusCode(200).extract().as(Pet.class);
 
-        Pet actualPetUsingGet = given().log().all().
+        Pet actualPetUsingGet = given().
                 pathParam("petId", pet.getId()).
                 when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{petId}").
-                then().log().all().statusCode(200).extract().as(Pet.class);
+                then().statusCode(200).extract().as(Pet.class);
 
 
 //        Tutaj celowo zmieniam dane aby wywołać błąd
